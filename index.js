8a6c44c85a6c9bd15b51d2f7c5015503f31456c5
Finite <33845166+finia2NA@users.noreply.github.com>
2024-04-26 18:50:25 +0200

    Actually return the data, add documentation

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+/**
+ * Retrieves the score for a given name from Metacritic.
+ * @param {string} name - The name to search for.
+ * @returns {Promise<string>} The score of the first search result. From my experience, either a number or "tbd".
+ * @throws {Error} If an error occurs during the retrieval process.
+ * 
+ * @example
+ * getScore('Cyberpunk 2077').then(console.log).catch(console.error);
+ */
 async function getScore(name) {
   const encodedName = name.replace(' ', '-');
   try {
     const response = await axios.get(`https://www.metacritic.com/search/${encodedName}`);
     const $ = cheerio.load(response.data);
     const firstDiv = $('.c-siteReviewScore').first().text();
-    console.log(firstDiv);
+    return firstDiv;
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 }
-
-// Example usage
-// getScore('Cyberpunk 2077');
 
 module.exports = getScore;
